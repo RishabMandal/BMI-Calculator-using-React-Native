56f2e3fe19af10287313ff5a3384d04ff8afa0a2
RishabMandal <rishab829@gmail.com>
2022-10-30 21:12:47 +0530

    Second update

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState("Your BMI will be displayed here");
+
+  let height
+  let weight
+  let bmi
+  function calculate() {
+    bmi = weight / height * height;
+    setText(bmi);
+    setText('10');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text className="text-purple-700 font-bold text-2xl mt-20" style={styles.text}>BMI CALCULATOR</Text>
+      <Text className="text-purple-700 bg-white w-11/12 py-14 text-center rounded-xl text-lg my-10" style={styles.text}>For advertisement</Text>
+      <TextInput className="text-purple-700 my-2 text-xl" onChange={calculate} placeholder='Enter height in cm' placeholderTextColor='white' />
+      <TextInput className="text-purple-700 my-2 text-xl" onChange={calculate} placeholder='Enter weight in kg' placeholderTextColor='white' />
+      <Text className="text-purple-700 bg-white w-11/12 py-14 text-center rounded-xl text-lg my-10" style={styles.text}>{text}</Text>
+      <Button className="text-red-600 rounded-xl" color="purple" style={styles.button} title='Calculate' />
     </View>
   );
 }
@@ -13,8 +30,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+
   },
+  text: {
+  },
+  button: {
+    // backgroundColor: 'purple',
+    // color: 'purple'
+  }
 });
